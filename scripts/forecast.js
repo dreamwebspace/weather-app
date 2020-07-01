8b4e53f533fdbd7da8b6265ed80433db9915f2cb
Martin <martin@dreamweb.space>
2020-07-01 13:18:18 +0100

    fetch cityy

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -4,9 +4,10 @@ const getCity = async (city)=> {
   const query = `?apikey=${key}&q=${city}`;
   const response = await fetch(base+query);
   const data = await response.json();
-  console.log(data);
+  return data[0];
   
 };
 
-getCity("manchester");
+getCity("London").then(data => console.log(data)).catch(err => console.log(err));
+
 
