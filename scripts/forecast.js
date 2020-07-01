4e3dda882e895dcd3f8c02d9dab52f1b26d08656
Martin <martin@dreamweb.space>
2020-07-01 16:24:51 +0100

    fetch data

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,13 +1,34 @@
 const key = "dIS6iRVTtsKdjMLWUsyXrHXzYRA7nEeQ";
+
+const getWeather = async (id) => {
+
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+return data[0];
+
+
+
+};
+
 const getCity = async (city)=> {
   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
+
   const response = await fetch(base+query);
   const data = await response.json();
   return data[0];
   
 };
 
-getCity("London").then(data => console.log(data)).catch(err => console.log(err));
+getCity("London").then(data => {
+  return getWeather(data.Key);
+}).then(data => {
+  console.log(data);
+}).catch(err => console.log(err));
+
 
 
